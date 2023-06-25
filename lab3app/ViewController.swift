80d79807ff1812f49877ff4555df2ea4813dc744
Timur Kharin <timurx404@gmail.com>
2023-06-25 16:01:30 +0300

    Initial Commit

---
+++ lab3app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab3app
+//
+//  Created by Timur Kharin on 25.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
