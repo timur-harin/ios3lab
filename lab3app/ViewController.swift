5bd17f05e1a9933fe5ded8d602a33ca7dfd161f9
Timur Kharin <timurx404@gmail.com>
2023-06-26 22:57:42 +0300

    init commit

--- lab3app/ViewController.swift
+++ lab3app/ViewController.swift
@@ -1,19 +1,159 @@
-//
-//  ViewController.swift
-//  lab3app
-//
-//  Created by Timur Kharin on 25.06.2023.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
+
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UITextField!
+    @IBOutlet weak var universityLabel: UITextView!
+    @IBOutlet weak var cityLabel: UITextView!
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let tapGestureBackground = UITapGestureRecognizer(target: self, action: #selector(self.backgroundTapped(_:)))
+        self.view.addGestureRecognizer(tapGestureBackground)
+        
+        
+        
+        avatarImageView.layer.cornerRadius = view.frame.width / 4
+        avatarImageView.clipsToBounds = true
+        
+        nameLabel.text = "Timur Harin"
+        universityLabel.text = "Innopolis University"
+        cityLabel.text = "Innopolis"
+        
+        
+        let borderLayer = CALayer()
+        borderLayer.backgroundColor =  UIColor.gray.cgColor
+        
+        borderLayer.frame = CGRect(origin: CGPoint(x:0.0, y: nameLabel.frame.size.height-1), size: CGSize(width: nameLabel.frame.size.width, height: 1.0))
+        
+
+
+        nameLabel.layer.addSublayer(borderLayer)
+        
+        if let name = nameLabel.text {
+            let initials = getInitials(name: nameLabel.text)
+            let avatarImage = imageWithInitials(initials: initials)
+            avatarImageView.image = avatarImage
+        }
+        
+        nameLabel.delegate=self
+        
+        
+                NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+                NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+                
+                let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+                view.addGestureRecognizer(tapGesture)
+
+    }
+    
+    func getInitials(name: String?) -> String{
+        let initials = name?.components(separatedBy: " ").reduce("") { ($0 == "" ? "" : "\($0.first ?? "A")") + "\($1.first ?? "A")" } ?? "AA"
+        return initials
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.nameLabel.endEditing(true)
+        return false
     }
 
+    
+    @objc func backgroundTapped(_ sender: UITapGestureRecognizer)
+    {
+        nameLabel.endEditing(true)
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+            if textField == nameLabel {
+                if let newName = textField.text {
+                    nameLabel.text = newName
+                    
+                    
+                    let initials = getInitials(name: nameLabel.text)
+                    let avatarImage = imageWithInitials(initials: initials)
+                    avatarImageView.image = avatarImage
+                }
+            }
+        }
+
+    @IBAction func universityButtonTapped(_ sender: UIButton) {
+        showInputDialog(title: "Enter University", message: nil, placeholder: "Your University") { newUniversity in
+            self.universityLabel.text = newUniversity
+        }
+    }
+
+    @IBAction func cityButtonTapped(_ sender: UIButton) {
+        showInputDialog(title: "Enter City", message: nil, placeholder: "Your City") { newCity in
+            self.cityLabel.text = newCity
+        }
+    }
+
+    private func imageWithInitials(initials: String) -> UIImage? {
+        let size = avatarImageView.frame.size
+        let frame = CGRect(origin: .zero, size: size)
+        let nameLabel = UILabel(frame: frame)
+        nameLabel.textAlignment = .center
+        nameLabel.textColor = .white
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        nameLabel.text = initials
+
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        if let context = UIGraphicsGetCurrentContext() {
+            nameLabel.layer.render(in: context)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return image
+        }
+        
+        return nil
+    }
+
+    private func showInputDialog(title: String, message: String?, placeholder: String, completion: @escaping (String?) -> Void) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = placeholder
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            let text = alertController.textFields?.first?.text
+            completion(text)
+        }
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+            if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+                scrollView.contentInset = contentInsets
+                scrollView.scrollIndicatorInsets = contentInsets
+            }
+        }
+        
+        @objc private func keyboardWillHide(_ notification: Notification) {
+            scrollView.contentInset = .zero
+            scrollView.scrollIndicatorInsets = .zero
+        }
+        
+        @objc private func dismissKeyboard() {
+            view.endEditing(true)
+        }
+        
+        deinit {
+            NotificationCenter.default.removeObserver(self)
+        }
 }
-
