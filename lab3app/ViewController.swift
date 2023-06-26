b590ea71db0ba25db321055c480c16a27d194917
Timur Kharin <timurx404@gmail.com>
2023-06-27 00:22:41 +0300

    solve keyboard issue

--- lab3app/ViewController.swift
+++ lab3app/ViewController.swift
@@ -1,15 +1,13 @@
 import UIKit
 
 class ViewController: UIViewController,UITextFieldDelegate {
-
+    
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var nameLabel: UITextField!
     @IBOutlet weak var universityLabel: UITextView!
     @IBOutlet weak var cityLabel: UITextView!
-    @IBOutlet weak var scrollView: UIScrollView!
-
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let tapGestureBackground = UITapGestureRecognizer(target: self, action: #selector(self.backgroundTapped(_:)))
@@ -30,11 +28,11 @@ class ViewController: UIViewController,UITextFieldDelegate {
         
         borderLayer.frame = CGRect(origin: CGPoint(x:0.0, y: nameLabel.frame.size.height-1), size: CGSize(width: nameLabel.frame.size.width, height: 1.0))
         
-
-
+        
+        
         nameLabel.layer.addSublayer(borderLayer)
         
-        if let name = nameLabel.text {
+        if nameLabel.text != nil {
             let initials = getInitials(name: nameLabel.text)
             let avatarImage = imageWithInitials(initials: initials)
             avatarImageView.image = avatarImage
@@ -42,7 +40,7 @@ class ViewController: UIViewController,UITextFieldDelegate {
         
         nameLabel.delegate=self
         
-
+        
     }
     
     
@@ -55,13 +53,13 @@ class ViewController: UIViewController,UITextFieldDelegate {
         self.nameLabel.endEditing(true)
         return false
     }
-
+    
     
     @objc func backgroundTapped(_ sender: UITapGestureRecognizer)
     {
         nameLabel.endEditing(true)
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
@@ -69,32 +67,32 @@ class ViewController: UIViewController,UITextFieldDelegate {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
     }
-
+    
     func textFieldDidEndEditing(_ textField: UITextField) {
-            if textField == nameLabel {
-                if let newName = textField.text {
-                    nameLabel.text = newName
-                    
-                    
-                    let initials = getInitials(name: nameLabel.text)
-                    let avatarImage = imageWithInitials(initials: initials)
-                    avatarImageView.image = avatarImage
-                }
+        if textField == nameLabel {
+            if let newName = textField.text {
+                nameLabel.text = newName
+                
+                
+                let initials = getInitials(name: nameLabel.text)
+                let avatarImage = imageWithInitials(initials: initials)
+                avatarImageView.image = avatarImage
             }
         }
-
+    }
+    
     @IBAction func universityButtonTapped(_ sender: UIButton) {
         showInputDialog(title: "Enter University", message: nil, placeholder: "Your University") { newUniversity in
             self.universityLabel.text = newUniversity
         }
     }
-
+    
     @IBAction func cityButtonTapped(_ sender: UIButton) {
         showInputDialog(title: "Enter City", message: nil, placeholder: "Your City") { newCity in
             self.cityLabel.text = newCity
         }
     }
-
+    
     private func imageWithInitials(initials: String) -> UIImage? {
         let size = avatarImageView.frame.size
         let frame = CGRect(origin: .zero, size: size)
@@ -103,7 +101,7 @@ class ViewController: UIViewController,UITextFieldDelegate {
         nameLabel.textColor = .white
         nameLabel.font = UIFont.boldSystemFont(ofSize: 24)
         nameLabel.text = initials
-
+        
         UIGraphicsBeginImageContextWithOptions(size, false, 0)
         if let context = UIGraphicsGetCurrentContext() {
             nameLabel.layer.render(in: context)
@@ -114,7 +112,7 @@ class ViewController: UIViewController,UITextFieldDelegate {
         
         return nil
     }
-
+    
     private func showInputDialog(title: String, message: String?, placeholder: String, completion: @escaping (String?) -> Void) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertController.addTextField { textField in
